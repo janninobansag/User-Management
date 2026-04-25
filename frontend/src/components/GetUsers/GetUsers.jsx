e81431d4feeefab44dbe0e74fa05ad2484fe79f8
khen <kheniethforthree3s@gmail.com>
2026-04-25 16:38:41 +0800

    initial update

--- frontend/src/components/GetUsers/GetUsers.jsx
+++ frontend/src/components/GetUsers/GetUsers.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
 import {useState, useEffect} from 'react'
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
+export const getUsers = () =>{
 
+}
+
+return(
+    <>
+    <div>
+        <label>
+            <h3>List of Users</h3>
+        </label>
+        {users.map((user,index))}
+    </div>
+    </>
+)
